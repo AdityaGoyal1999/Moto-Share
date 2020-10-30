d57f53caa84161cdbab8312fefe804fcdbe52f0c
Julien <jbertazzolambert@gmail.com>
2020-10-29 22:23:39 -0500

    Added website hero to home page

---
+++ src/react-components/Hero/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './style.css'
+
+class Hero extends React.Component {
+  render () {
+    return (
+      <div id='hero'>
+        <h1>Motorcycles When You Need Them.</h1>
+      </div>
+    )
+  }
+}
+
+export default Hero
--- src/react-components/HomePage/index.js
+++ src/react-components/HomePage/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
 import NavBar from '../NavBar'
+import Hero from '../Hero'
 
 class HomePage extends React.Component {
   render () {
     return (
       <div>
         <NavBar loggedIn={false} />
+        <Hero />
       </div>
     )
   }
