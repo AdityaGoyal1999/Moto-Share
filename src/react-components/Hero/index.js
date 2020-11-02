08006ed785ff07bc779f0180aec8f8c28b53a966
Julien <jbertazzolambert@gmail.com>
2020-11-02 13:05:33 -0600

    Added background image to hero div

--- src/react-components/Hero/index.js
+++ src/react-components/Hero/index.js
@@ -6,7 +6,7 @@ class Hero extends React.Component {
   render () {
     return (
       <div id='hero'>
-        <h1>Motorcycles When You Need Them.</h1>
+        <h1>Project Name <br /> Motorcycles When You Need Them.</h1>
       </div>
     )
   }
