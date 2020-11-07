5c5cb49e375b040fc14f47e6f728b65a73a94b59
Julien <jbertazzolambert@gmail.com>
2020-11-06 22:09:46 -0600

    NavBar now has view for when logged in

--- src/react-components/HomeCards/index.js
+++ src/react-components/HomeCards/index.js
@@ -8,28 +8,28 @@ class HomeCards extends React.Component {
     return (
       <div id='cardsContainer'>
         <Card className='card'>
-          <CardMedia class='media' image='./book.jpg' />
+          <CardMedia className='media' image='./book.jpg' />
           <CardContent>
             <h2>Book a bike</h2>
             <p>Find a bike and book it using site credits</p>
           </CardContent>
         </Card>
         <Card className='card'>
-          <CardMedia class='media' image='./pickup.jpg' />
+          <CardMedia className='media' image='./pickup.jpg' />
           <CardContent>
             <h2>Pick up your ride</h2>
             <p>Decide on a place to meet with your<br /> bike lord and pick up your ride there</p>
           </CardContent>
         </Card>
         <Card className='card'>
-          <CardMedia class='media' image='./return.jpg' />
+          <CardMedia className='media' image='./return.jpg' />
           <CardContent>
             <h2>Return your ride</h2>
             <p>On the return date, bring the<br /> bike back to the bike lord</p>
           </CardContent>
         </Card>
         <Card className='card'>
-          <CardMedia class='media' image='./bikelord.jpg' />
+          <CardMedia className='media' image='./bikelord.jpg' />
           <CardContent>
             <h2>Want to earn credits?</h2>
             <p>Post ads and rent out your bike</p>
--- src/react-components/NavBar/index.js
+++ src/react-components/NavBar/index.js
@@ -1,26 +1,59 @@
 import React from 'react'
 import './style.css'
-import { Button, ButtonGroup, AppBar, Toolbar, Typography } from '@material-ui/core'
+import { Button, ButtonGroup, AppBar, Toolbar, Typography, Menu, MenuItem } from '@material-ui/core'
 
 class NavBar extends React.Component {
-  render () {
-    const renderLoginArea = () => {
-      if (!this.props.loggedIn) {
-        return (
-          <ButtonGroup id='login'>
-            <Button href='/signup'>Sign Up</Button>
-            <Button href='/login'>Log In</Button>
-          </ButtonGroup>
-        )
-      }
-    }
+  state = {
+    anchorEl: null,
+  }
 
+  openMenu = (event) => {
+    this.setState({
+      anchorEl: document.querySelector('#menuAnchor')
+    })
+  }
+
+  closeMenu = () => {
+    this.setState({
+      anchorEl: null
+    })
+  }
+
+  logout = () => {
+    this.setState({
+      username: null
+    })
+  }
+
+  renderLoginArea = () => {
+    if (!this.state.username) {
+      return (
+        <ButtonGroup id='login'>
+          <Button href='/signup'>Sign Up</Button>
+          <Button href='/login'>Log In</Button>
+        </ButtonGroup>
+      )
+    } else {
+      return (
+        <div>
+          {/* SAMPLE will be replaced by username */}
+          <Button id='menuAnchor' onClick={this.openMenu}>SAMPLE</Button>
+          <Menu anchorEl={this.state.anchorEl} keepMounted open={Boolean(this.state.anchorEl)} onClose={this.closeMenu}>
+            <MenuItem><a href='/AccountInfo'>My Account</a></MenuItem>
+            <MenuItem onClick={this.logout}>Log Out</MenuItem>
+          </Menu>
+        </div>
+      )
+    }
+  }
+
+  render () {
     return (
       <AppBar>
         <Toolbar id='navbar'>
-          <Typography variant='h6'>Project Name</Typography>
+          <Typography variant='h6'><a href='/'>Project Name</a></Typography>
           <Button>About Us</Button>
-          {renderLoginArea()}
+          {this.renderLoginArea()}
         </Toolbar>
       </AppBar>
     )
