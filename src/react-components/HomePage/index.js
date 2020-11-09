a063edff9f3a3de947cd3be6373b28713f3a7755
Julien <jbertazzolambert@gmail.com>
2020-11-08 23:33:37 -0600

    Edited home page look and feel

--- src/react-components/HomePage/index.js
+++ src/react-components/HomePage/index.js
@@ -4,16 +4,16 @@ import NavBar from '../NavBar'
 import Hero from '../Hero'
 import SearchBox from '../SearchBox'
 import HomeCards from '../HomeCards'
+import './style.css'
 
 // The main page (at '/')
 class HomePage extends React.Component {
   render () {
     return (
-      <div>
+      <div id='homeContainer'>
         <NavBar loggedIn={false} />
         <Hero />
         <SearchBox />
-        <h2>How it works: </h2>
         <HomeCards />
       </div>
     )
