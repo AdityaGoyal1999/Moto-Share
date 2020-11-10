6753c5d740045958ed8c8a213a1e96d7e9ec1cfb
rutavshah <rutavshah@users.noreply.github.com>
2020-11-09 19:41:57 -0500

    Added admin tables using materials ui

--- src/react-components/AdminDataTableView/index.js
+++ src/react-components/AdminDataTableView/index.js
@@ -1,82 +1,63 @@
-import React from 'react'
-import './style.css'
+import React from 'react';
+import './style.css';
+import ReactDOM from 'react-dom';
+import UserTable from './UserTable';
+import AdTable from './AdTable';
+import BookingsTable from './BookingsTable';
+import NavBar from '../NavBar';
+import Button from '@material-ui/core/Button';
 
 class AccountInfo extends React.Component {
+    constructor(item) {
+        super(item);
+        this.state = {
+            usertable: true,
+            adtable: false,
+            borrowedtable: false
+        };
+    }
+
+    utable = () => {
+        this.setState({ usertable: true, adtable: false, borrowedtable: false });
+    };
+
+    atable = () => {
+        this.setState({ usertable: false, adtable: true, borrowedtable: false });
+    };
+
+    btable = () => {
+        this.setState({ usertable: false, adtable: false, borrowedtable: true });
+    };
+
   render(){
     return(
-        <div>
-          <div id='picContainer'>
-            <img id ='profilePic' src={require('./Profilepic.jpg')} alt= "profile pic"/>
-          </div>
-            <table id = "datatable">
-                <tr>
-                    <th id = 'item'>Firstname</th>
-                    <th id = 'item'>Lastname</th>
-                    <th id = 'item'>Age</th>
-                </tr>
-                <tr>
-                    <td id = 'item'>Jill</td>
-                    <td id = 'item'>Smith</td>
-                    <td id = 'item'>50</td>
-                </tr>
-                <tr>
-                    <td id = 'item'>Eve</td>
-                    <td id = 'item'>Jackson</td>
-                    <td id = 'item'>94</td>
-                </tr>
-                <tr>
-                    <td id = 'item'>Jill</td>
-                    <td id = 'item'>Smith</td>
-                    <td id = 'item'>50</td>
-                </tr>
-                <tr>
-                    <td id = 'item'>Eve</td>
-                    <td id = 'item'>Jackson</td>
-                    <td id = 'item'>94</td>
-                </tr>
-                <tr>
-                    <td id = 'item'>Jill</td>
-                    <td id = 'item'>Smith</td>
-                    <td id = 'item'>50</td>
-                </tr>
-                <tr>
-                    <td id = 'item'>Eve</td>
-                    <td id = 'item'>Jackson</td>
-                    <td id = 'item'>94</td>
-                </tr>
-                <tr>
-                    <td id = 'item'>Jill</td>
-                    <td id = 'item'>Smith</td>
-                    <td id = 'item'>50</td>
-                </tr>
-                <tr>
-                    <td id = 'item'>Eve</td>
-                    <td id = 'item'>Jackson</td>
-                    <td id = 'item'>94</td>
-                </tr>
-                <tr>
-                    <td id = 'item'>Jill</td>
-                    <td id = 'item'>Smith</td>
-                    <td id = 'item'>50</td>
-                </tr>
-                <tr>
-                    <td id = 'item'>Eve</td>
-                    <td id = 'item'>Jackson</td>
-                    <td id = 'item'>94</td>
-                </tr>
-                <tr>
-                    <td id = 'item'>Jill</td>
-                    <td id = 'item'>Smith</td>
-                    <td id = 'item'>50</td>
-                </tr>
-                <tr>
-                    <td id = 'item'>Eve</td>
-                    <td id = 'item'>Jackson</td>
-                    <td id = 'item'>94</td>
-                </tr>
+        <div id='page'>
+            <NavBar />
+            <div id='container'>
+                <div id='picContainer'>
+                    <img id ='profilePic' src={require('./Profilepic.jpg')} alt= "profile pic"/>
+                </div>
 
+                <div id='TableContainer'>
+                    <div>
+                        <Button onClick={this.utable}> Users Table</Button>
+                        <Button onClick={this.atable}> Ads</Button>
+                        <Button onClick={this.btable}> Bookings</Button>
+                    </div>
+                    <div id='TableTitleContainer'>
 
-            </table>
+                        {this.state.usertable ? <h2 id='TableTitle'> Users </h2>: null}
+                        {this.state.adtable ? <h2 id='TableTitle'> Ads </h2>: null}
+                        {this.state.borrowedtable ?  <h2 id='TableTitle'> Bookings </h2>: null}
+
+                    </div>
+                    <div id='table'>
+                        {this.state.usertable ? <UserTable/>: null}
+                        {this.state.adtable ? <AdTable/>: null}
+                        {this.state.borrowedtable ? <BookingsTable/>: null}
+                    </div>
+                </div>
+            </div>
         </div>
     )
   }
