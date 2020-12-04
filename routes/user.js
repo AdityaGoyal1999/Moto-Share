cf95c71fd177232ce11407001a4723a164d17a48
Julien <jbertazzolambert@gmail.com>
2020-12-04 13:26:35 -0600

    Most routes done
    still need to handle adding/removing bike

--- routes/user.js
+++ routes/user.js
@@ -6,10 +6,10 @@ const router = express.Router()
 const { mongoose } = require('../db/mongoose')
 const { ObjectID } = require('mongodb')
 const { User } = require('../models/user')
-const bcrypt = require('bcryptjs')
 
 // body-parser: middleware for parsing HTTP JSON body into a usable object
 const bodyParser = require('body-parser')
+const user = require('../models/user')
 router.use(bodyParser.json())
 
 function isMongoError (error) { // checks for first error returned by promise rejection if Mongo database suddently disconnects
@@ -62,7 +62,7 @@ router.get('/api/users', mongoChecker, (req, res) => {
 router.post('/api/users', mongoChecker, (req, res) => {
   const newUser = new User({
     email: req.body.email,
-    password: req.body.email,
+    password: req.body.password,
     name: req.body.name,
     location: '',
     rating: -1,
@@ -91,9 +91,65 @@ router.post('/api/users', mongoChecker, (req, res) => {
 router.get('/api/users/:id', mongoChecker, idChecker, (req, res) => {
   User.findById(req.params.id).then(result => {
     if (!result) {
-      res.status(404).send('Resource not found')
+      res.status(404).send('resource not found')
     } else {
       res.send(result)
+    }
+  }).catch(error => {
+    log(error)
+    if (isMongoError(error)) {
+      res.status(500).send('internal server error')
+    } else {
+      res.status(400).send('bad request')
+    }
+  })
+})
+
+// leave a review for a user
+// expects:
+// {
+//   rating: rating given by review,
+//   review: 'the review'
+// }
+router.patch('/api/users/:id/reviews', mongoChecker, idChecker, (req, res) => {
+  User.findById(req.params.id).then(result => {
+    if (!result) {
+      res.status(404).send('resource not found')
+    } else {
+      // update average rating and add new review
+      result.rating = (result.rating * result.reviews.length + req.body.rating) / (result.reviews.length + 1)
+      result.reviews.push(req.body.review)
+      result.save()
+      res.send({review: {
+        rating: req.body.rating,
+        review: req.body.review
+      }, user: result})
+    }
+  }).catch(error => {
+    log(error)
+    if (isMongoError(error)) {
+      res.status(500).send('internal server error')
+    } else {
+      res.status(400).send('bad request')
+    }
+  })
+})
+
+// update user image
+// expects:
+// {
+//   image_id: 'image id',
+//   image_url: 'image url'
+// }
+router.patch('/api/users/:id/image', mongoChecker, idChecker, (req, res) => {
+  User.findByIdAndUpdate(req.params.id, {
+    $set: req.body
+  }, { new: true }).then(result => {
+    if (!result) {
+      res.status(404).send('resource not found')
+    } else {
+      result.save()
+      res.send({image: req.body, user: result})
     }
   }).catch(error => {
     log(error)
@@ -111,19 +167,29 @@ router.get('/api/users/:id', mongoChecker, idChecker, (req, res) => {
 //   email: 'email address',
 //   password: 'password'
 // }
-router.get('/api/userByEmail', mongoChecker, async (req, res) => {
-  User.findOne({
-    email: req.body.email
-  }).then(result => {
-    if (!result) {
-      res.status(404).send('Resource not found')
+router.get('/api/userByEmail', mongoChecker, (req, res) => {
+  // call the thingy
+  User.findByEmailPassword(req.body.email, req.body.password).then(result => {
+    res.send(result)
+  }).catch(error => {
+    log(error)
+    if (isMongoError(error)) {
+      res.status(500).send('internal server error')
+    } else if (error && error.message === '404') {
+      res.status(404).send('resource not found')
     } else {
-      const foundPass = result.password
-      bcrypt.compare(foundPass, req.body.password).then(bcryptResult => {
-        res.send(result)
-      }).catch(error => {
-        log(error)
-      })
+      res.status(400).send('bad request')
+    }
+  })
+})
+
+// delete a user
+router.delete('/api/users/:id', mongoChecker, idChecker, (req, res) => {
+  User.findByIdAndDelete(req.params.id).then(result => {
+    if (!result) {
+      res.status(404).send('resource not found')
+    } else {
+      res.send(result)
     }
   }).catch(error => {
     log(error)
